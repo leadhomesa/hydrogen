e2557937aceebbf25007d5a9c8430fa5fc301d98
Blessing Jonamu <blessing.jonamu@gmail.com>
2020-02-26 11:11:22 +0200

    feat: expose setAuthToken

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 export { listingUrl, slug } from './listing-url';
 export { jwtParser } from './jwt';
 export { identify, page, track } from './segment';
-export { getAuthToken, isLoggedIn, getBearer, logout } from './auth';
 export { affordabilityCalculator, repaymentCalculator } from './bonds';
+export {
+  getAuthToken,
+  setAuthToken,
+  isLoggedIn,
+  getBearer,
+  logout
+} from './auth';
